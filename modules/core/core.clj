049b33f08a72c05995d84937ebd7d7aa909abefa
olu sogbesan <sogbesano@gmail.com>
2017-03-16 19:22:09 +0000

    formatting of code of core.clj of modules updated

--- modules/core/core.clj
+++ modules/core/core.clj
@@ -1,6 +1,6 @@
 (ns modules.core.core
-  (:require [irc-command.command :refer [new-privmsg-cmd]]
-            [clojure.string :refer [join, replace, includes?]])
+  (:require [irc-command.command :refer [new-privmsg-cmd]])
+  (:require [clojure.string :refer [join, replace, includes?]])
   (:require [irc-bot.core :refer [loaded-modules]])
   (:require [module-command.command :refer [get-loaded-modules-keys]])
   (:gen-class)
@@ -9,20 +9,24 @@
 (defn caw
   "CAW cmd yields a PRIVMSG where the final arg is a random length crow onomatopoeia in japanese kana unicode"
   []
-  (let [
-         rng (rand-int 50) 
-         jp-kana-crow-onomatopoeia (join " " (repeat (max 2 rng) "\u304B\u30FC!"))
-       ] 
-    (new-privmsg-cmd "#beepboop" jp-kana-crow-onomatopoeia))
+  (let 
+    [
+      rng (rand-int 50) 
+      jp-kana-crow-onomatopoeia (join " " (repeat (max 2 rng) "\u304B\u30FC!"))
+    ] 
+    (new-privmsg-cmd "#beepboop" jp-kana-crow-onomatopoeia)
+  )
 )
 
 (defn botsnack
   "BOTSNACK cmd yields a PRIVMSG where the final arg is itadakimasu in japanese hiragana unicode"
   []
-  (let [
-         jp-hiragana-itadakimasu "\u3044\u305F\u3060\u304D\u307E\u3059!"
-       ]
-  (new-privmsg-cmd "#beepboop" jp-hiragana-itadakimasu))
+  (let 
+    [
+      jp-hiragana-itadakimasu "\u3044\u305F\u3060\u304D\u307E\u3059!"
+    ]
+    (new-privmsg-cmd "#beepboop" jp-hiragana-itadakimasu)
+  )
 )
 
 (defn ls-cmds
@@ -34,7 +38,8 @@
       all-module-cmds-comma-seperated (replace (join ", " all-loaded-module-cmds) #":" "")
       all-module-cmds-fmtd (format "[%s]" all-module-cmds-comma-seperated)
     ]
-    (new-privmsg-cmd "#beepboop" all-module-cmds-fmtd))
+    (new-privmsg-cmd "#beepboop" all-module-cmds-fmtd)
+  )
 )
 
 (defn cmd-help
@@ -45,7 +50,9 @@
         cmd-help-msg "cmd-help requires one argument, the module command name. Example usage: %cmd-help -botsnack. 
                          Use module command ls-cmds to see a list of available module commands."
       ] 
-      (new-privmsg-cmd "#beepboop" cmd-help-msg)))
+      (new-privmsg-cmd "#beepboop" cmd-help-msg)
+    )
+  )
 
   ([module-cmd-name]
     (let
@@ -56,7 +63,10 @@
       ]
       (if loaded-module-cmd?
         "" ;;get module command help description
-        (new-privmsg-cmd "#beepboop" (format "%s is not a valid loaded module command" module-cmd-name-hyphen-rmd)))))
+        (new-privmsg-cmd "#beepboop" (format "%s is not a valid loaded module command." module-cmd-name-hyphen-rmd))
+      )
+    )
+  )
 )
   
 (def module-name-and-cmds 
