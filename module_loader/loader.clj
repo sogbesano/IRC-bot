0911843cfec7521fca132519d67670bd6b1de5b6
olu sogbesan <sogbesano@gmail.com>
2017-03-16 21:20:36 +0000

    code reformatted in loader.clj of module_loader

--- module_loader/loader.clj
+++ module_loader/loader.clj
@@ -1,18 +1,25 @@
 (ns module-loader.loader
   (:require [clojure.java.io :refer [file]])
-  (:gen-class))
+  (:gen-class)
+)
 
 (defn module?
   "Returns true if module-name exists in modules dir"
   [module-name]
-  (let [dir (file (format "%s/src/modules/%s" (.getCanonicalPath (file ".")) module-name))]
-    (.exists dir)))
+  (let 
+    [
+      dir (file (format "%s/src/modules/%s" (.getCanonicalPath (file ".")) module-name))
+    ]
+    (.exists dir)
+  )
+)
 
 (defn load-module
   "Loads a module"
   [module-name]
   (if (module? module-name)
     (var-get (load-file (format "%s/src/modules/%s/%s.clj" (.getCanonicalPath (file ".")) module-name module-name)))
-    (println (format "Unable to find module: %s" module-name)))
+    (println (format "Unable to find module: %s" module-name))
   )
+)
 
