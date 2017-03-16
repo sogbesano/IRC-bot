ce4a614e0dbe22ff6bc7230136025498376aa0f3
olu sogbesan <sogbesano@gmail.com>
2017-03-16 21:01:12 +0000

    -main function code reformatted in core.clj of irc_bot

--- irc_bot/core.clj
+++ irc_bot/core.clj
@@ -25,31 +25,34 @@
     (init-connection sock nick username channel)
     (println (format "LOADED MODULES: %s" (vec (map (fn [module] (:module-name module)) loaded-modules))))
     (let
-      [running? true]
-        (while running?
-          (let 
-            [
-              rx-irc-cmd (read-irc-cmd sock) 
-              parsed-rx-irc-cmd (parse-irc-cmd rx-irc-cmd)
-              final-arg (:final-arg parsed-rx-irc-cmd)
-              module-prompt "%"
-            ]
-            (println (format "PARSED RX IRC CMD: %s" parsed-rx-irc-cmd))
-            (if (starts-with-module-prompt? final-arg module-prompt)
-              (let 
-                [
-                   module-cmd (:cmd-name (parse-module-cmd final-arg))
-                   module-cmd-args (:cmd-args (parse-module-cmd final-arg))
-                ]
-                (if (module-cmd? module-cmd loaded-modules)
-                  (send-irc-cmd sock (invoke-module-cmd module-cmd module-cmd-args loaded-modules))
-                  (println (format "%s, is not a valid module command." module-cmd))
-                )
+      [
+        running? true
+      ]
+      (while running?
+        (let 
+          [
+            rx-irc-cmd (read-irc-cmd sock) 
+            parsed-rx-irc-cmd (parse-irc-cmd rx-irc-cmd)
+            final-arg (:final-arg parsed-rx-irc-cmd)
+            module-prompt "%"
+          ]
+          (println (format "PARSED RX IRC CMD: %s" parsed-rx-irc-cmd))
+          (if (starts-with-module-prompt? final-arg module-prompt)
+            (let 
+              [
+                module-cmd (:cmd-name (parse-module-cmd final-arg))
+                module-cmd-args (:cmd-args (parse-module-cmd final-arg))
+              ]
+              (if (module-cmd? module-cmd loaded-modules)
+                (send-irc-cmd sock (invoke-module-cmd module-cmd module-cmd-args loaded-modules))
+                (println (format "%s, is not a valid module command." module-cmd))
               )
             )
           )
         )
+      )
     )
+  )
 )
 
 
