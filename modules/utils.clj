39beea6222684f479a3927084f571b6f6f403c3f
olu sogbesan <sogbesano@gmail.com>
2017-03-16 21:24:05 +0000

    code reformatted in utils.clj of modules

--- modules/utils.clj
+++ modules/utils.clj
@@ -1,8 +1,10 @@
 (ns modules.utils
   (:require [clojure.string :refer [starts-with?]])
-  (:gen-class))
+  (:gen-class)
+)
 
 (defn starts-with-module-prompt?
   "Returns true if final-arg of parsed-rx-irc-cmd starts with module prompt"
   [final-arg, module-prompt]
-  (starts-with? (if (nil? final-arg) "" final-arg) module-prompt))
+  (starts-with? (if (nil? final-arg) "" final-arg) module-prompt)
+)
